66e9d13eae6ae78078c9a284292075ec7b418863
Samar4eg <thebulychev@gmail.com>
2022-12-09 15:32:16 +0000

    Updates

---
+++ static/r3IKZy7PyAjq8-ZZTOkOo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/971-220a5fd54b73bf41.js","static/chunks/pages/index-37361d4a5a60e5d4.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
